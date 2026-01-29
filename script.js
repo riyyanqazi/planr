41233402e7098d875fb57662206941859e8bcaba
riyyanqazi <riyyanqazii@gmail.com>
2026-01-29 15:28:01 +0500

    New changes

--- script.js
+++ script.js
@@ -83,4 +83,12 @@ const faqs = document.querySelectorAll(".faq-item");
     });
     
 
-    
+      var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
